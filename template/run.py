0e114bed311542966c4a46f5eb7a823f39a281be
Julien Flack <julienflack@gmail.com>
2024-06-23 11:48:36 +0800

    update

--- template/run.py
+++ template/run.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
+from template.schemas import InputSchema
 from naptha_sdk.utils import get_logger, load_yaml
 
 logger = get_logger(__name__)
 
-def run(inputs, worker_nodes=None, orchestrator_node=None, flow_run=None, cfg=None):
+def run(inputs: InputSchema, worker_nodes=None, orchestrator_node=None, flow_run=None, cfg=None):
     logger.info(f"Inputs: {inputs}")
     return None
 
 if __name__ == "__main__":
+
     cfg_path = "template/component.yaml"
     cfg = load_yaml(cfg_path)
-    inputs = {"prompt": "hi"}
+
+    inputs = InputSchema(
+        param1="template",
+        param2="naptha",
+    )
     run(inputs, cfg=cfg)
