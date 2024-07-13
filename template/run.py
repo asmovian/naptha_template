9817a9485284db8af64b4c3347c349366cce3d80
Julien Flack <julienflack@gmail.com>
2024-07-13 12:07:48 +0800

    hooked up to llm

--- template/run.py
+++ template/run.py
@@ -3,15 +3,26 @@ from template.schemas import InputSchema
 from naptha_sdk.utils import get_logger, load_yaml
 import chromadb
 import json
-
+import asyncio
+from ollama import AsyncClient
 
 logger = get_logger(__name__)
 
+async def ollama_chat(host, model, messages):
+    return await AsyncClient(host=host).chat(model=model, messages=messages)
+
+
 def run(inputs: InputSchema, worker_nodes=None, orchestrator_node=None, flow_run=None, cfg=None):
     logger.info(f"Inputs: {inputs}")
+    logger.debug(f"config = {cfg}")
 
     client = chromadb.PersistentClient(path=inputs.input_dir)
-    collection_name = "roko-telegram"
+    collection_name = cfg["chroma"]["collection"]
+
+    # Set the prompt
+    messages = [
+        {"role": "system", "content": cfg["inputs"]["system_message"]}
+    ]
 
     collections = client.list_collections()
     existing_collection_names = [x.name for x in collections]
@@ -20,14 +31,28 @@ def run(inputs: InputSchema, worker_nodes=None, orchestrator_node=None, flow_run
         num = f"{collection_name} has {collection.count()} entries"
         logger.info(num)
 
-        r = collection.query(query_texts=inputs.question, n_results=5)
-        ret_str = json.dumps(r["documents"], indent=2)
+        # put vector db results into query:
+        results = collection.query(query_texts=inputs.question, n_results=10)
+        for doc in results["documents"][0]:
+            messages.append({"role": "assistant", "content": doc})
+
     else:
-        ret_str = f"Error: Collection {collection_name} not found."
+        logger.warning(f"Error: Collection {collection_name} not found.")
 
-    logger.debug(ret_str)
+    messages.append({"role": "user", "content": inputs.question})
 
-    return ret_str
+    logger.debug(messages)
+
+    output = asyncio.run(
+        ollama_chat(cfg["models"]["ollama"]["api_base"], 
+                    cfg["models"]["ollama"]["model"], 
+                    messages
+        )
+    )
+
+    logger.debug(output)
+
+    return output
 
 
 if __name__ == "__main__":
@@ -35,6 +60,11 @@ if __name__ == "__main__":
     cfg_path = "template/component.yaml"
     cfg = load_yaml(cfg_path)
 
+    # Note: this is for local testing only. When running on naptha
+    # it may look like this:
+    # naptha write_storage -i chromadb.zip
+    # -> storage id for file system = <id>: i.e. something like 1caddaae8ca5495c996db80e33e42da9
+    # naptha run template -p "question='Where should I store my Roko coins?' input_dir='<id>'"
     inputs = InputSchema(
         question="Where should I store my Roko coin?",
         input_dir="/home/julien/data/naptha/roko-chromadb"
